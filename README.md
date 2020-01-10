499d1b187d82d648c01a67a2d0b6cea5905325cd
Kevin <kevin.pasteur@cpnv.ch>
2020-01-10 10:22:37 +0100

    Add files

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-E-count
+Nogake_Test
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('add','CarController@create');
+Route::post('add','CarController@store');
+Route::get('car','CarController@index');
+Route::get('edit/{id}','CarController@edit');
+Route::post('edit/{id}','CarController@update');
+Route::delete('{id}','CarController@destroy');
